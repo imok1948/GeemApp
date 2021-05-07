8882b0d574bee9cde9d3688207e1c36203b5637d
unknown <divss38@gmail.com>
2021-05-07 12:12:06 +0530

    changes1

--- app/src/main/java/com/example/geem/fragments/profile/FragmentProfile.java
+++ app/src/main/java/com/example/geem/fragments/profile/FragmentProfile.java
@@ -7,10 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.geem.R;
-
 
 
 import android.content.Intent;
@@ -69,14 +69,15 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
 
  //Related to Register section
  ConstraintLayout registerFragment;
- TextView registerBannerDescription;
+ ImageView registerBannerDescription;
  EditText registerFullName, registerBirthYear, registerEmail, registerPassword;
  Button registerRegisterUser, registerGoBackToLogin;
  ProgressBar registerProgressBar;
 
  //Related to ForgotPassword section
  ConstraintLayout passwordFragment;
- TextView passwordBanner, passwordBannerDescription;
+
+ ImageView passwordBanner;
  EditText passwordEmail;
  Button passwordResetPassword, passwordGoBackToLogin;
  ProgressBar passwordProgressBar;
@@ -91,6 +92,112 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
 
  //String for EditText Purpose
  private String temp;
+
+
+ void hideLoginScreen()
+ {
+  loginFragment.setVisibility(View.GONE);
+  loginRegister.setVisibility(View.GONE);
+  loginForgotPassword.setVisibility(View.GONE);
+  loginSignIn.setVisibility(View.GONE);
+  loginEmail.setVisibility(View.GONE);
+  loginPassword.setVisibility(View.GONE);
+  loginProgressBar.setVisibility(View.GONE);
+ }
+
+ void showLoginScreen()
+ {
+  loginFragment.setVisibility(View.VISIBLE);
+  loginRegister.setVisibility(View.VISIBLE);
+  loginForgotPassword.setVisibility(View.VISIBLE);
+  loginSignIn.setVisibility(View.VISIBLE);
+  loginEmail.setVisibility(View.VISIBLE);
+  loginPassword.setVisibility(View.VISIBLE);
+  loginProgressBar.setVisibility(View.VISIBLE);
+ }
+
+
+ void hideRegisterScreen()
+ {
+  registerFragment.setVisibility(View.GONE);
+  registerBannerDescription.setVisibility(View.GONE);
+  registerFullName.setVisibility(View.GONE);
+  registerBirthYear.setVisibility(View.GONE);
+  registerEmail.setVisibility(View.GONE);
+  registerPassword.setVisibility(View.GONE);
+  registerRegisterUser.setVisibility(View.GONE);
+  registerGoBackToLogin.setVisibility(View.GONE);
+  registerProgressBar.setVisibility(View.GONE);
+ }
+
+ void showRegisterScreen()
+ {
+  registerFragment.setVisibility(View.VISIBLE);
+  registerBannerDescription.setVisibility(View.VISIBLE);
+  registerFullName.setVisibility(View.VISIBLE);
+  registerBirthYear.setVisibility(View.VISIBLE);
+  registerEmail.setVisibility(View.VISIBLE);
+  registerPassword.setVisibility(View.VISIBLE);
+  registerRegisterUser.setVisibility(View.VISIBLE);
+  registerGoBackToLogin.setVisibility(View.VISIBLE);
+  registerProgressBar.setVisibility(View.VISIBLE);
+ }
+
+
+ void hideForgetScreen()
+ {
+  passwordFragment.setVisibility(View.GONE);
+  passwordBanner.setVisibility(View.GONE);
+
+  passwordEmail.setVisibility(View.GONE);
+  passwordResetPassword.setVisibility(View.GONE);
+  passwordGoBackToLogin.setVisibility(View.GONE);
+  passwordProgressBar.setVisibility(View.GONE);
+ }
+
+
+ void showForgetScreen()
+ {
+  passwordFragment.setVisibility(View.VISIBLE);
+  passwordBanner.setVisibility(View.VISIBLE);
+
+  passwordEmail.setVisibility(View.VISIBLE);
+  passwordResetPassword.setVisibility(View.VISIBLE);
+  passwordGoBackToLogin.setVisibility(View.VISIBLE);
+  passwordProgressBar.setVisibility(View.VISIBLE);
+ }
+
+ void hideProfileScreen()
+ {
+  userProfile.setVisibility(View.GONE);
+  userProfileFullName.setVisibility(View.GONE);
+  userProfileEmail.setVisibility(View.GONE);
+  userProfileBirthYear.setVisibility(View.GONE);
+  userProfileSignOut.setVisibility(View.GONE);
+ }
+
+ void showProfileScreen()
+ {
+  userProfile.setVisibility(View.VISIBLE);
+  userProfileFullName.setVisibility(View.VISIBLE);
+  userProfileEmail.setVisibility(View.VISIBLE);
+  userProfileBirthYear.setVisibility(View.VISIBLE);
+  userProfileSignOut.setVisibility(View.VISIBLE);
+ }
+
+
+ void hideLoginScreen2()
+ {
+
+
+  loginProgressBar.setVisibility(View.GONE);
+  loginFragment.setVisibility(View.GONE);
+  registerFragment.setVisibility(View.GONE);
+  passwordFragment.setVisibility(View.GONE);
+
+  userProfile.setVisibility(View.VISIBLE);
+ }
+
 
  @Override
  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -109,7 +216,7 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
   loginForgotPassword = view.findViewById(R.id.loginForgotPassword);
   registerBannerDescription = view.findViewById(R.id.registerBannerDescription);
   passwordBanner = view.findViewById(R.id.passwordBanner);
-  passwordBannerDescription = view.findViewById(R.id.passwordBannerDescription);
+
   userProfileFullName = view.findViewById(R.id.userProfileFullName);
   userProfileEmail = view.findViewById(R.id.userProfileEmail);
   userProfileBirthYear = view.findViewById(R.id.userProfileBirthYear);
@@ -145,19 +252,24 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
   //Declare String value (for overwriting EditText)
   temp = null;
 
+
   //onClickListeners in Login section
-  loginRegister.setOnClickListener(new View.OnClickListener() {
+  loginRegister.setOnClickListener(new View.OnClickListener()
+  {
    @Override
-   public void onClick(View v) {
+   public void onClick(View v)
+   {
     loginFragment.setVisibility(View.GONE);
     registerFragment.setVisibility(View.VISIBLE);
     passwordFragment.setVisibility(View.GONE);
    }
   });
 
-  loginForgotPassword.setOnClickListener(new View.OnClickListener() {
+  loginForgotPassword.setOnClickListener(new View.OnClickListener()
+  {
    @Override
-   public void onClick(View v) {
+   public void onClick(View v)
+   {
     loginFragment.setVisibility(View.GONE);
     registerFragment.setVisibility(View.GONE);
     passwordFragment.setVisibility(View.VISIBLE);
@@ -165,9 +277,11 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
   });
 
 
-  passwordGoBackToLogin.setOnClickListener(new View.OnClickListener() {
+  passwordGoBackToLogin.setOnClickListener(new View.OnClickListener()
+  {
    @Override
-   public void onClick(View v) {
+   public void onClick(View v)
+   {
     loginFragment.setVisibility(View.VISIBLE);
     registerFragment.setVisibility(View.GONE);
     passwordFragment.setVisibility(View.GONE);
@@ -176,24 +290,30 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
    }
   });
 
-  loginSignIn.setOnClickListener(new View.OnClickListener() {
+  loginSignIn.setOnClickListener(new View.OnClickListener()
+  {
    @Override
-   public void onClick(View v) {
+   public void onClick(View v)
+   {
     userLogin();
    }
   });
 
   //onClickListeners in Register section
-  registerRegisterUser.setOnClickListener(new View.OnClickListener() {
+  registerRegisterUser.setOnClickListener(new View.OnClickListener()
+  {
    @Override
-   public void onClick(View v) {
+   public void onClick(View v)
+   {
     registerUser();
    }
   });
 
-  registerGoBackToLogin.setOnClickListener(new View.OnClickListener() {
+  registerGoBackToLogin.setOnClickListener(new View.OnClickListener()
+  {
    @Override
-   public void onClick(View v) {
+   public void onClick(View v)
+   {
     loginFragment.setVisibility(View.VISIBLE);
     registerFragment.setVisibility(View.GONE);
     passwordFragment.setVisibility(View.GONE);
@@ -202,28 +322,68 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
 
 
   //onClickListeners in Password section
-  passwordResetPassword.setOnClickListener(new View.OnClickListener() {
+  passwordResetPassword.setOnClickListener(new View.OnClickListener()
+  {
    @Override
-   public void onClick(View v) {
+   public void onClick(View v)
+   {
     resetPassword();
    }
   });
 
   //onClickListeners in User Profile section
-  userProfileSignOut.setOnClickListener(new View.OnClickListener() {
+  userProfileSignOut.setOnClickListener(new View.OnClickListener()
+  {
    @Override
-   public void onClick(View v) {
+   public void onClick(View v)
+   {
     loginEmail.setText(temp);
     loginPassword.setText(temp);
     loginFragment.setVisibility(View.VISIBLE);
     registerFragment.setVisibility(View.GONE);
     passwordFragment.setVisibility(View.GONE);
     userProfile.setVisibility(View.GONE);
+
+    mAuth.signOut();
    }
   });
 
 
+  boolean loggedIn = true;
 
+  if(mAuth.getCurrentUser() != null && mAuth.getCurrentUser().getUid() != null)
+  {
+   currentUserID = mAuth.getCurrentUser().getUid();
+   hideLoginScreen2();
+   loggedIn = true;
+   DocumentReference documentReference = db.collection("profile1").document(currentUserID);
+
+   documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>()
+   {
+    @Override
+    public void onComplete(@NonNull Task<DocumentSnapshot> task)
+    {
+     if(task.isSuccessful())
+     {
+      DocumentSnapshot snapshot = task.getResult();
+
+      userProfileFullName.setText(snapshot.getString("name"));
+      userProfileEmail.setText(snapshot.getString("email"));
+      userProfileBirthYear.setText(snapshot.getString("birthYear"));
+     }
+     else
+     {
+      //Retrieving user details failed
+     }
+    }
+   });
+
+  }
+  else
+  {
+   //Already logged in
+
+  }
 
 
   //Toast.makeText(getContext(), getArguments().getString(Variables.GREETING_KEY), Toast.LENGTH_SHORT).show();
@@ -231,34 +391,40 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
  }
 
  @Override
- public void onClick(View v) {
+ public void onClick(View v)
+ {
 
  }
 
  //userLogin() starts
- private void userLogin() {
+ private void userLogin()
+ {
   String email = loginEmail.getText().toString().trim();
   String password = loginPassword.getText().toString().trim();
 
-  if(email.isEmpty()){
+  if(email.isEmpty())
+  {
    loginEmail.setError("Email is required!");
    loginEmail.requestFocus();
    return;
   }
 
-  if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+  if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+  {
    loginEmail.setError("Please enter a valid email!");
    loginEmail.requestFocus();
    return;
   }
 
-  if(password.isEmpty()){
+  if(password.isEmpty())
+  {
    loginPassword.setError("Password is required!");
    loginPassword.requestFocus();
    return;
   }
 
-  if(password.length() < 6){
+  if(password.length() < 6)
+  {
    loginPassword.setError("Minimum password length is 6 characters!");
    loginPassword.requestFocus();
    return;
@@ -267,14 +433,18 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
   loginProgressBar.setVisibility(View.VISIBLE);
 
 
-  mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+  mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+  {
    @Override
-   public void onComplete(@NonNull Task<AuthResult> task) {
+   public void onComplete(@NonNull Task<AuthResult> task)
+   {
 
-    if(task.isSuccessful()){
+    if(task.isSuccessful())
+    {
      FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-     if(user.isEmailVerified()){
+     if(user.isEmailVerified())
+     {
       //redirect to user profile
       loginProgressBar.setVisibility(View.GONE);
       loginFragment.setVisibility(View.GONE);
@@ -283,10 +453,13 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
 
       //Retrieve the logged in user information from the Cloud FireStore
       currentUserID = mAuth.getCurrentUser().getUid();
+
       DocumentReference documentReference = db.collection("profile1").document(currentUserID);
-      documentReference.addSnapshotListener(getActivity(), new EventListener<DocumentSnapshot>() {
+      documentReference.addSnapshotListener(getActivity(), new EventListener<DocumentSnapshot>()
+      {
        @Override
-       public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+       public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error)
+       {
         userProfileFullName.setText(value.getString("name"));
         userProfileEmail.setText(value.getString("email"));
         userProfileBirthYear.setText(value.getString("birthYear"));
@@ -295,13 +468,17 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
       //Data Retrieval Completed
 
       userProfile.setVisibility(View.VISIBLE);
-     }else{
+     }
+     else
+     {
       user.sendEmailVerification();
       Toast.makeText(getActivity(), "Check your email to verify your account!", Toast.LENGTH_LONG).show();
      }
 
 
-    }else{
+    }
+    else
+    {
      Toast.makeText(getActivity(), "Failed to login! Please check your credentials!", Toast.LENGTH_LONG).show();
      loginProgressBar.setVisibility(View.GONE);
     }
@@ -312,49 +489,59 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
  //userLogin() ends
 
  //registerUser() starts
- private void registerUser() {
+ private void registerUser()
+ {
   String email = registerEmail.getText().toString().trim();
   String password = registerPassword.getText().toString().trim();
   String fullName = registerFullName.getText().toString().trim();
   String birthYear = registerBirthYear.getText().toString().trim();
 
-  if(fullName.isEmpty()){
+  if(fullName.isEmpty())
+  {
    registerFullName.setError("Full name is required!");
    registerFullName.requestFocus();
    return;
   }
-  if(birthYear.isEmpty()){
+  if(birthYear.isEmpty())
+  {
    registerBirthYear.setError("Age is required!");
    registerBirthYear.requestFocus();
    return;
   }
-  if(fullName.isEmpty()){
+  if(fullName.isEmpty())
+  {
    registerEmail.setError("Email is required!");
    registerEmail.requestFocus();
    return;
   }
-  if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+  if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+  {
    registerEmail.setError("Please provide valid email!");
    registerEmail.requestFocus();
    return;
   }
-  if(password.isEmpty()){
+  if(password.isEmpty())
+  {
    registerPassword.setError("Password is required!");
    registerPassword.requestFocus();
    return;
   }
-  if(password.length() < 6){
+  if(password.length() < 6)
+  {
    registerPassword.setError("Minimum password length should be 6 characters!");
    registerPassword.requestFocus();
    return;
   }
 
   registerProgressBar.setVisibility(View.VISIBLE);
-  mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+  mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+  {
    @Override
-   public void onComplete(@NonNull Task<AuthResult> task) {
+   public void onComplete(@NonNull Task<AuthResult> task)
+   {
     registerProgressBar.setVisibility(View.VISIBLE);
-    if(task.isSuccessful()){
+    if(task.isSuccessful())
+    {
      loginEmail.setText(temp);
      loginPassword.setText(temp);
      Toast.makeText(getActivity(), "User has been registered successfully!", Toast.LENGTH_LONG).show();
@@ -371,7 +558,9 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
      registerBirthYear.setText(temp);
      registerEmail.setText(temp);
      registerPassword.setText(temp);
-    }else{
+    }
+    else
+    {
      Toast.makeText(getActivity(), "Failed to register! Try again!", Toast.LENGTH_LONG).show();
      registerProgressBar.setVisibility(View.GONE);
     }
@@ -383,29 +572,37 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
  //registerUser() ends
 
  //resetPassword() starts
- private void resetPassword() {
+ private void resetPassword()
+ {
   String email = passwordEmail.getText().toString().trim();
 
-  if(email.isEmpty()){
+  if(email.isEmpty())
+  {
    passwordEmail.setError("Email is required!");
    passwordEmail.requestFocus();
    return;
   }
 
-  if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+  if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+  {
    passwordEmail.setError("Please provide a valid email!");
    passwordEmail.requestFocus();
    return;
   }
 
   passwordProgressBar.setVisibility(View.VISIBLE);
-  mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+  mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>()
+  {
    @Override
-   public void onComplete(@NonNull Task<Void> task) {
+   public void onComplete(@NonNull Task<Void> task)
+   {
 
-    if(task.isSuccessful()){
+    if(task.isSuccessful())
+    {
      Toast.makeText(getActivity(), "Check your email to reset the password!", Toast.LENGTH_LONG).show();
-    }else{
+    }
+    else
+    {
      Toast.makeText(getActivity(), "Try again! Something wrong happened!", Toast.LENGTH_LONG).show();
     }
    }
@@ -422,24 +619,25 @@ public class FragmentProfile extends Fragment implements View.OnClickListener
   items.put("email", registerEmail.getText().toString().trim());
   items.put("birthYear", registerBirthYear.getText().toString());
 
-  db.collection("profile1").document(currentUserID)
-          .set(items)
-          .addOnSuccessListener(new OnSuccessListener<Void>() {
-           @Override
-           public void onSuccess(Void aVoid) {
-            Log.d(TAG, "DocumentSnapshot for user successfully written!");
-            registerProgressBar.setVisibility(View.GONE);
-            Toast.makeText(getActivity(), "Data Inserted Successfully!", Toast.LENGTH_SHORT).show();
-           }
-          })
-          .addOnFailureListener(new OnFailureListener() {
-           @Override
-           public void onFailure(@NonNull Exception e) {
-            Log.w(TAG, "Error writing document for user", e);
-            Toast.makeText(getActivity(), "Failed to register! Try again!", Toast.LENGTH_LONG).show();
-            registerProgressBar.setVisibility(View.GONE);
-           }
-          });
+  db.collection("profile1").document(currentUserID).set(items).addOnSuccessListener(new OnSuccessListener<Void>()
+  {
+   @Override
+   public void onSuccess(Void aVoid)
+   {
+    Log.d(TAG, "DocumentSnapshot for user successfully written!");
+    registerProgressBar.setVisibility(View.GONE);
+    Toast.makeText(getActivity(), "Data Inserted Successfully!", Toast.LENGTH_SHORT).show();
+   }
+  }).addOnFailureListener(new OnFailureListener()
+  {
+   @Override
+   public void onFailure(@NonNull Exception e)
+   {
+    Log.w(TAG, "Error writing document for user", e);
+    Toast.makeText(getActivity(), "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+    registerProgressBar.setVisibility(View.GONE);
+   }
+  });
  }
  //insertData() ends
 
